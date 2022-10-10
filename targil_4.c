76ebc91c9192a1776576e9bd9cb29e9cf921b5c8
GelfondAnswers <115484758+GelfondAnswers@users.noreply.github.com>
2022-10-10 19:14:31 +0300

    Update targil_4.c

--- targil_4.c
+++ targil_4.c
@@ -1,35 +1,22 @@
+int four_two(int *p, int len){
+    int *j = p;
+    int a, b, c = 0;
+    a = *j;
+    j += 1;
 
-int four_two(int* j){
-    // lo aia li koh likro et kol atext az asiti et za
-    int *p = j;
-    int a, b, c1 = 0, c2 = 0; // c1 is for list, c2 is for num of elems
-    a = *p;
-    p += 1;
-    while(*p != NULL){
-        b = *p;
-        //explanation: i store the lengths of sorted sublists as numbers in an int
+    for(int i = 1; i < len; i ++){
+        b = *j;
         if(a <= b)
-            c1 += 1;
+            c+=1;
         else
-            c1 *= 10;
-        a = b;
-        c2 += 1;
-        p +=1;
-    }
-
-    int l;
-
-    while(c1 > 0){
-        l = c1 % 10;
-        if(l > c2 / 2)
+            c = 0;
+        if(c >= len/2)
             return 1;
-        c1 = c1 / 10;
+        a = b;
+        j += 1;
     }
-
-
     return 0;
 }
-
 int main(){
   int m[] = {1, 2, 3, 2};
     //printf("%d", 4%10);
